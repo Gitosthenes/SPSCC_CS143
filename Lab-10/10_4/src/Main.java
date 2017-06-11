1bae6b7445f7d8821ff4e12c9bbc4cbc3b753eb2
NegroKetchum <negro.ketchum.github@gmail.com>
2017-06-10 22:41:13 -0700

    Finish Lab-10.

--- Lab-10/10_4/src/Main.java
+++ Lab-10/10_4/src/Main.java
@@ -3,191 +3,137 @@
 import java.util.*;
 
 public class Main {
-    int[] winningState = {0,0,3,3,1,0,0};
-    boolean isFirstMove = true;
-    Queue<String> steps;
 
-    public static void main(String[] args) {
+    private static ArrayList<int[]> previousStates  = new ArrayList<>();
+    private static int[] winningState = {0,0,1,3,3};
+    private static int fails = 0;
 
-    }
-
-    //Helper method that sets up the starting state and queue to hold steps
-    void solve() {
-        int[] startingState = {3,3,0,0,0,0,0};
-        steps = new ArrayDeque<>();
+    /**
+     * Helper method that sets up the starting state and queue to hold steps.
+     */
+    private static void solve() {
+        int[] startingState = {3,3,0,0,0};
+        Stack<String> steps = new Stack<>();
         solve(startingState, steps);
     }
 
-    //Recursively tries every possible move based on the state of the puzzle at a given time.
-    //If a move is possible, it is added to the queue of steps.
-    void solve(int[] state, Queue<String> steps) {
-        //Check for base case (win condition)
+    /**
+     * Recursively tries every possible move based on the state of the puzzle at a given time.
+     * If a move is possible, it is added to the queue of steps, then prints the steps when a solution is found.
+     */
+    private static void solve(int[] state, Stack<String> steps) {
+        //Base case (win condition)
         if (Arrays.equals(state, winningState)) {
-            while (!steps.isEmpty()){
-                steps.poll();
+            Stack<String> winningSteps = new Stack<>();
+            while (!steps.isEmpty()) {
+                winningSteps.push(steps.pop());
             }
-        //Check for base case (lose condition)
+            while (!winningSteps.isEmpty()) {
+                System.out.println(winningSteps.pop());
+            }
+            System.out.println("Everyone's across in one piece!");
+            System.out.println("\nThis program hit a dead end " + fails + " times before it solved the riddle.");
+            System.exit(0);
+        //Base case (lose condition)
         } else if (isIllegalState(state)) {
-            steps.poll();
-        //Didn't win or lose so make a move
+            fails++;
+        //Recursive case (make a move)
         } else {
-            //Change first move boolean value so loops can be caught.
-            isFirstMove = false;
+            int[] move;
+            previousStates.add(state);
             //Checks which side of the river the boat is on.
-            switch (state[4]) {
-                //Goes down this path when boat is on starting side of the river.
+            switch (state[2]) {
+                //Case for starting side of the river.
                 case 0:
-                    //Checks if the boat has anybody in it
-                    if (boatHasPeople(state)) {
-                        //Check if vegetarian is in 1st spot on boat
-                        if(state[5] == -1) {
-                            //Check if vegetarian in 2nd spot on boat
-                            if (state[6] == -1) {
+                    //Make all possible moves:
+                    steps.add("One vegetarian crosses the river.");
+                    move = new int[]{state[0]-1,state[1],1,state[3]+1,state[4]};
+                    solve(move, steps);
+                    steps.pop();
 
-                            //Check if meat eater in 2nd spot on boat
-                            } else if (state[6] == 1) {
+                    steps.add("One meat eater crosses the river.");
+                    move = new int[]{state[0],state[1]-1,1,state[3],state[4]+1};
+                    solve(move, steps);
+                    steps.pop();
 
-                            //2nd spot on boat must be empty
-                            } else {
+                    steps.add("Two vegetarians cross the river.");
+                    move = new int[]{state[0]-2,state[1],1,state[3]+2,state[4]};
+                    solve(move, steps);
+                    steps.pop();
 
-                            }
-                        //Checks if meat eater in 1st spot on boat.
-                        } else if(state[5] == 1) {
-                            //Check if vegetarian is in 2nd spot on boat
-                            if(state[6] == -1) {
+                    steps.add("Two meat eaters cross the river.");
+                    move = new int[]{state[0],state[1]-2,1,state[3],state[4]+2};
+                    solve(move, steps);
+                    steps.pop();
 
-                            //Check if meat eater is in 2nd spot on boat.
-                            } else if (state[6] == 1) {
-
-                            //2nd spot must be empty
-                            } else {
-
-                            }
-                        //The 1st spot in the boat must be empty
-                        } else {
-                            //Checks if vegetarian is in 2nd spot on boat
-                            if(state[6] == -1) {
-
-                            //Must be a meat eater in the 2nd spot
-                            } else {
-
-                            }
-                        }
-                    //The boat must be empty
-                    } else {
-                        //Try all possible moves:
-                        steps.add("One vegetarian crosses on the boat.");
-                        int[] move = {2,3,0,0,1,-1,0};
-                        solve(move, steps);
-
-                        steps.add("Two vegetarians cross on the boat.");
-                        move = new int[]{1,3,0,0,1,-1,-1};
-                        solve(move, steps);
-
-                        steps.add("One meat eater crosses on the boat.");
-                        move = new int[]{3,2,0,0,1,1,0};
-                        solve(move, steps);
-
-                        steps.add("Two meat eaters cross on the boat.");
-                        move = new int[]{3,1,0,0,1,1,1};
-                        solve(move, steps);
-
-                        steps.add("One vegetarian and one meat eater cross on the boat.");
-                        move = new int[]{2,2,0,0,1,-1,1};
-                        solve(move, steps);
-                    }
+                    steps.add("One vegetarian and one meat eater cross the river.");
+                    move = new int[]{state[0]-1,state[1]-1,1,state[3]+1,state[4]+1};
+                    solve(move, steps);
+                    steps.pop();
                     break;
-                //Goes down this path if the boat is on the goal side of the river.
+                //Case for goal side of the river.
                 case 1:
-                //Checks if the boat has anybody in it
-                if (boatHasPeople(state)) {
-                    //Check if vegetarian is in 1st spot on boat
-                    if(state[5] == -1) {
-                        //Check if vegetarian in 2nd spot on boat
-                        if (state[6] == -1) {
-
-                            //Check if meat eater in 2nd spot on boat
-                        } else if (state[6] == 1) {
-
-                            //2nd spot on boat must be empty
-                        } else {
-
-                        }
-                        //Checks if meat eater in 1st spot on boat.
-                    } else if(state[5] == 1) {
-                        //Check if vegetarian is in 2nd spot on boat
-                        if(state[6] == -1) {
-
-                            //Check if meat eater is in 2nd spot on boat.
-                        } else if (state[6] == 1) {
-
-                            //2nd spot must be empty
-                        } else {
-
-                        }
-                        //The 1st spot in the boat must be empty
-                    } else {
-                        //Checks if vegetarian is in 2nd spot on boat
-                        if(state[6] == -1) {
-
-                            //Must be a meat eater in the 2nd spot
-                        } else {
-
-                        }
-                    }
-                    //The boat must be empty
-                } else {
-                    //Try all possible moves:
-                    steps.add("One vegetarian crosses on the boat.");
-                    int[] move = {2,3,0,0,0,-1,0};
+                    //Make all possible moves:
+                    steps.add("One vegetarian crosses back over the river.");
+                    move = new int[]{state[0]+1,state[1],0,state[3]-1,state[4]};
                     solve(move, steps);
+                    steps.pop();
 
-                    steps.add("Two vegetarians cross on the boat.");
-                    move = new int[]{1,3,0,0,0,-1,-1};
+                    steps.add("One meat eater crosses back over the river.");
+                    move = new int[]{state[0],state[1]+1,0,state[3],state[4]-1};
                     solve(move, steps);
+                    steps.pop();
 
-                    steps.add("One meat eater crosses on the boat.");
-                    move = new int[]{3,2,0,0,0,1,0};
+                    steps.add("Two vegetarians cross back over the river.");
+                    move = new int[]{state[0]+2,state[1],0,state[3]-2,state[4]};
                     solve(move, steps);
+                    steps.pop();
 
-                    steps.add("Two meat eaters cross on the boat.");
-                    move = new int[]{3,1,0,0,0,1,1};
+                    steps.add("Two meat eaters cross back over the river.");
+                    move = new int[]{state[0],state[1]+2,0,state[3],state[4]-2};
                     solve(move, steps);
+                    steps.pop();
 
-                    steps.add("One vegetarian and one meat eater cross on the boat.");
-                    move = new int[]{2,2,0,0,0,-1,1};
+                    steps.add("One vegetarian and one meat eater cross back over the river.");
+                    move = new int[]{state[0]+1,state[1]+1,0,state[3]-1,state[4]-1};
                     solve(move, steps);
-                }
+                    steps.pop();
                     break;
             }
         }
     }
-    //TODO: Add all other illegal states and make sure boat can't cross river by itself.
+
     /**
-     * Checks current state of puzzle for lose conditions, eg:
+     * Checks current state of puzzle for lose conditions, e.g.:
      *  - Meat eaters outnumber vegetarians on either side where vegetarians are present.
-     *  - All meat eaters and vegetarians end up back on the starting side (the puzzle has looped).
+     *  - Everybody is in the same positions they were in at a  (the puzzle has looped).
      *  - There are a negative number of vegetarians or meat eaters on either side.
      *  - There are more than 3 vegetarians or meat eaters on either side.
      */
-
-    boolean isIllegalState(int[] state) {
-        if (state[1] > state[0] && state[0] != 0) {
-            return true;
-        } else if (state[3] > state[2] && state[2] != 0) {
-            return true;
-        } else if (state[0] == 3 && state[1] == 0 && isFirstMove == false) {
-            return true;
-        } else {
-            return false;
-        }
+    private static boolean isIllegalState(int[] state) {
+        return isPreviousState(state) ||
+               state[1] > state[0] && state[0] > 0 ||
+               state[4] > state[3] && state[3] > 0 ||
+               state[0] > 3 ||
+               state[0] < 0 ||
+               state[1] > 3 ||
+               state[1] < 0;
     }
 
-    boolean boatHasPeople(int[] state) {
-        if (state[5] != 0 || state[6] != 0) {
-            return true;
-        } else {
-            return false;
+    /**
+     * Checks whether the current state of the puzzle is identical to
+     * any of the previous moves made, indicating a loop.
+     */
+    private static boolean isPreviousState(int[] currentState) {
+        for (int[] previousState : previousStates) {
+            if (Arrays.equals(currentState, previousState)) {
+                return true;
+            }
         }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        solve();
     }
 }
